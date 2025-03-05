7f2cc58260ef71c51c8955ba0cabd06ddc0bf2d5
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-03-05 17:20:29 +0200

    new

--- src/app/List/page.tsx
+++ src/app/List/page.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import Link from "next/link";
+// import React from "react";
+// import Link from "next/link";
 
-// Define the prop type for List
-interface ListProps {
-  listProps: string[];
-}
+// // Define the prop type for List
+// interface ListProps {
+//   listProps: string[];
+// }
 
-// Use List as the default export
-const List: React.FC<ListProps> = ({ listProps }) => {
-  return (
-    <ul className="bg-gray-400 z-2 absolute text-center w-35 list-container">
-      {listProps.map((item) => (
-        <li className="hover:bg-purple-400 list" key={item}>
-          <Link href={item}>{item}</Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+// // Use List as the default export
+// const List: React.FC<ListProps> = ({ listProps }) => {
+//   return (
+//     <ul className="bg-gray-400 z-2 absolute text-center w-35 list-container">
+//       {listProps.map((item) => (
+//         <li className="hover:bg-purple-400 list" key={item}>
+//           <Link href={item}>{item}</Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
 
-export default List;
+// export default List;
